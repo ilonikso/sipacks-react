e6c09e6893e879912d310c4871695071324c70d7
Illia Onishchenko <pearljam546@gmail.com>
2020-10-05 10:21:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>This is the start of Sipacks on React</p>
+    </div>
+  );
+}
+
+export default App;
